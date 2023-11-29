28437ccf90e00d7cf57c54d082b44ec6ce2774a5
sreytouch <atouch.it@gmail.com>
2023-11-29 13:25:22 -0800

    Initial Commit

---
+++ CalculatorSwiftUI/CalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftUIApp.swift
+//  CalculatorSwiftUI
+//
+//  Created by Sreytouch(Jessica) on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
